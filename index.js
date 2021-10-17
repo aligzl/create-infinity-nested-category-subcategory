909a8b9118009da3d55fab4e6ee8f3dd21b71019
Ali Güzel <mudreamworld@gmail.com>
2021-10-17 12:13:44 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,415 @@
+const  dropdown = [
+        {
+          name: "Satılık",
+          icon: "ci-tag",
+          id: "satilik",
+          children: [
+            {
+              name: "Konut",
+              id: "konut",
+              icon: "icon-cottage",
+              children: [
+                {
+                  id: "daire",
+                  name: "Daire",
+                },
+                {
+                  id: "eski-ev",
+                  name: "Eski Ev",
+                },
+                {
+                  id: "konak",
+                  name: "Konak",
+                },
+                {
+                  id: "kooperatif",
+                  name: "Kooperatif",
+                },
+                {
+                  id: "kosk",
+                  name: "Köşk",
+                },
+                {
+                  id: "mustakil-ev",
+                  name: "Müstakil Ev",
+                },
+                {
+                  id: "prefabrik-ev",
+                  name: "Prefabrik Ev",
+                },
+                {
+                  id: "residance",
+                  name: "Residance",
+                },
+                {
+                  id: "villa",
+                  name: "Villa",
+                },
+                {
+                  id: "yazlik",
+                  name: "Yazlık",
+                },
+                {
+                  id: "ciftlik-evi",
+                  name: "Çiftlik Evi",
+                },
+              ],
+            },
+            {
+              name: "İş Yeri",
+              id: "is-yeri",
+              icon: "icon-shop",
+              children: [
+                {
+                  id: "daire",
+                  name: "Daire",
+                },
+                {
+                  id: "eski-ev",
+                  name: "Eski Ev",
+                },
+                {
+                  id: "mustakil-ev",
+                  name: "Müstakil Ev",
+                },
+                {
+                  id: "konak",
+                  name: "Konak",
+                },
+                {
+                  id: "villa",
+                  name: "Villa",
+                },
+                {
+                  id: "residance",
+                  name: "Residance",
+                },
+                {
+                  id: "ciftlik-evi",
+                  name: "Çiftlik Evi",
+                },
+                {
+                  id: "kosk",
+                  name: "Köşk",
+                },
+                {
+                  id: "yazlik",
+                  name: "Yazlık",
+                },
+                {
+                  id: "kooperatif",
+                  name: "Kooperatif",
+                },
+              ],
+            },
+            {
+              name: "Arsa",
+              id: "arsa",
+              icon: "icon-fence",
+              children: [],
+            },
+          ],
+        },
+        {
+          name: "Kiralık",
+          icon: "ci-lable",
+          id: "kiralik",
+          children: [
+            {
+              name: "Konut",
+              id: "konut",
+              icon: "icon-cottage",
+              children: [
+                {
+                  id: "daire",
+                  name: "Daire",
+                },
+                {
+                  id: "eski-ev",
+                  name: "Eski Ev",
+                },
+                {
+                  id: "mustakil-ev",
+                  name: "Müstakil Ev",
+                },
+                {
+                  id: "konak",
+                  name: "Konak",
+                },
+                {
+                  id: "villa",
+                  name: "Villa",
+                },
+                {
+                  id: "residance",
+                  name: "Residance",
+                },
+                {
+                  id: "ciftlik-evi",
+                  name: "Çiftlik Evi",
+                },
+                {
+                  id: "kosk",
+                  name: "Köşk",
+                },
+                {
+                  id: "yazlik",
+                  name: "Yazlık",
+                },
+                {
+                  id: "kooperatif",
+                  name: "Kooperatif",
+                },
+              ],
+            },
+            {
+              name: "İş Yeri",
+              id: "is-yeri",
+              icon: "icon-shop",
+              children: [
+                {
+                  id: "akaryakit-istasyonu",
+                  name: "Akaryakıt İstasyonu",
+                },
+                {
+                  id: "apartman-dairesi",
+                  name: "Apartman Dairesi",
+                },
+                {
+                  id: "atolye",
+                  name: "Atölye",
+                },
+                {
+                  id: "avm",
+                  name: "Avm",
+                },
+                {
+                  id: "bufe",
+                  name: "Büfe",
+                },
+                {
+                  id: "buro-veya-ofis",
+                  name: "Büro veya Ofis",
+                },
+                {
+                  id: "cafe-ve-bar",
+                  name: "Cafe ve Bar",
+                },
+                {
+                  id: "ciftlik",
+                  name: "Çiftlik",
+                },
+                {
+                  id: "depo-veya-antrepo",
+                  name: "Depo veya Antrepo",
+                },
+                {
+                  id: "dugun-salonu",
+                  name: "Düğün Salonu",
+                },
+                {
+                  id: "dukkan-ve-magaza",
+                  name: "Dükkan ve Mağaza",
+                },
+                {
+                  id: "eczane-ve-medikal",
+                  name: "Eczane ve Medikal",
+                },
+                {
+                  id: "fabrika",
+                  name: "Fabrika",
+                },
+                {
+                  id: "fotograf-studyosu",
+                  name: "Fotoğraf Stüdyosu",
+                },
+                {
+                  id: "garaj",
+                  name: "Garaj",
+                },
+                {
+                  id: "garaj-ve-park-yeri",
+                  name: "Garaj ve Park Yeri",
+                },
+                {
+                  id: "imalathane",
+                  name: "İmalathane",
+                },
+                {
+                  id: "is-hani-kati-veya-ofisi",
+                  name: "iş Hanı Katı veya Ofisi",
+                },
+                {
+                  id: "kantin",
+                  name: "Kantin",
+                },
+                {
+                  id: "kir-ve-kahvalti-bahcesi",
+                  name: "Kır ve Kahvaltı Bahçesi",
+                },
+                {
+                  id: "kiraathane",
+                  name: "Kıraathane",
+                },
+                {
+                  id: "komple-bina",
+                  name: "Komple Bina",
+                },
+                {
+                  id: "kuafor-ve-guzellik-merkezi",
+                  name: "Kuaför ve Güzellik merkezi",
+                },
+                {
+                  id: "maden-ocagi",
+                  name: "Maden Ocağı",
+                },
+                {
+                  id: "market",
+                  name: "Market",
+                },
+                {
+                  id: "muayenehane",
+                  name: "Muayenehane",
+                },
+                {
+                  id: "ofis",
+                  name: "Ofis",
+                },
+                {
+                  id: "okul-ve-kurs",
+                  name: "Okul ve Kurs",
+                },
+                {
+                  id: "oto-yikama-ve-kuafor",
+                  name: "Oto Yıkama ve Kuaför",
+                },
+                {
+                  id: "otopark",
+                  name: "Otopark",
+                },
+                {
+                  id: "pastane-ve-firin",
+                  name: "Pastane ve Fırın",
+                },
+                {
+                  id: "pazar-yeri",
+                  name: "Pazar Yeri",
+                },
+                {
+                  id: "plaza",
+                  name: "Plaza",
+                },
+                {
+                  id: "plaza-kati",
+                  name: "Plaza Katı",
+                },
+                {
+                  id: "prefabrik-yapi",
+                  name: "Prefabrik Yapı",
+                },
+                {
+                  id: "radyo-istasyonu",
+                  name: "Radyo İstasyonu",
+                },
+                {
+                  id: "residnce-kati-ve-ofisi",
+                  name: "Residnce Katı ve Ofisi",
+                },
+                {
+                  id: "restoran-ve-lokant",
+                  name: "Restoran ve Lokant",
+                },
+                {
+                  id: "saglik-merkezi",
+                  name: "Sağlık Merkezi",
+                },
+                {
+                  id: "sanal-ofis",
+                  name: "Sanal Ofis",
+                },
+                {
+                  id: "sinema-ve-konferans-salonu",
+                  name: "Sinema ve Konferans Salonu",
+                },
+                {
+                  id: "spa,-hamam-ve-sauna",
+                  name: "SPA, Hamam ve Sauna",
+                },
+                {
+                  id: "spor-tesisi",
+                  name: "Spor Tesisi",
+                },
+                {
+                  id: "taksi-duragi",
+                  name: "Taksi Durağı",
+                },
+                {
+                  id: "tamirhane",
+                  name: "Tamirhane",
+                },
+                {
+                  id: "yurt",
+                  name: "Yurt",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "Günlük Kiralık",
+          id: "gunluk-kiralik",
+          icon: "ci-lable",
+          children: [
+            {
+              name: "Daire",
+              id: "daire",
+              icon: "icon-building",
+              children: [],
+            },
+            {
+              name: "Residance",
+              id: "residance",
+              icon: "icon-apartment",
+              children: [],
+            },
+            {
+              name: "Müstakil Ev",
+              id: "mustakil-ev",
+              icon: "icon-house-private",
+              children: [],
+            },
+            {
+              name: "Villa",
+              id: "villa",
+              icon: "icon-villa",
+              children: [],
+            },
+            {
+              name: "Köşk ve Konak",
+              id: "kosk-ve-konak",
+              icon: "icon-mansion",
+              children: [],
+            },
+            {
+              name: "Yazlık",
+              id: "yazlik",
+              icon: "icon-summery",
+              children: [],
+            },
+          ],
+        },
+      ]
+
+    const  reduceArrayDimension = (array, parent = '') => {
+      const level = [];
+
+      array.forEach(item => {
+        const ids = {name: item.name, id: item.id,  parent: `${parent}/${item.id}`}
+        level.push(ids);
+        if (item.children) level.push(...reduceArrayDimension(item.children, ids.parent));
+      });
+
+      return level;
+    }
+
+    console.log(reduceArrayDimension(dropdown))
